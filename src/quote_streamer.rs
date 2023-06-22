8b472839ebfccf8d6c2882ac11945be3ca8f794d
MCHSL <michal.sul+githubalt@wp.pl>
2023-06-22 03:10:15 +0200

    create subscriptions instead of having just one

--- src/quote_streamer.rs
+++ src/quote_streamer.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+use std::mem::ManuallyDrop;
 use std::sync::Mutex;
 use std::{ffi::CString, fmt::Display};
 
-use dxfeed::Event;
+use dxfeed::{dxf_event_flags_t, Event};
 use widestring::WideCString;
 
 use crate::api::order::AsSymbol;
@@ -21,16 +23,78 @@ impl Display for DxFeedError {
     }
 }
 
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub struct SubscriptionId(usize);
+
+#[derive(Debug)]
+pub struct QuoteSubscription {
+    pub id: SubscriptionId,
+    subscription: dxfeed::dxf_subscription_t,
+    event_receiver: flume::Receiver<dxfeed::Event>,
+    sender_ptr: *mut flume::Sender<dxfeed::Event>,
+}
+
+impl Drop for QuoteSubscription {
+    fn drop(&mut self) {
+        if !self.subscription.is_null() {
+            assert_eq!(SUCCESS, unsafe {
+                dxfeed::dxf_close_subscription(self.subscription)
+            });
+            self.subscription = std::ptr::null_mut();
+            unsafe {
+                std::mem::drop(Box::from_raw(self.sender_ptr));
+            }
+        }
+    }
+}
+
+impl QuoteSubscription {
+    pub fn add_symbols<S: AsSymbol>(&self, symbols: &[S]) {
+        let symbols: Vec<WideCString> = symbols
+            .iter()
+            .map(|sym| WideCString::from_str(sym.as_symbol().0).unwrap())
+            .collect();
+
+        let mut symbol_pointers: Vec<*const i32> = symbols
+            .iter()
+            .map(|sym| sym.as_ptr() as *const i32)
+            .collect();
+
+        let c_syms: *mut dxfeed::dxf_const_string_t =
+            symbol_pointers.as_mut_slice().as_ptr() as *mut dxfeed::dxf_const_string_t;
+
+        assert_eq!(SUCCESS, unsafe {
+            dxfeed::dxf_add_symbols(self.subscription, c_syms, symbols.len() as i32)
+        });
+    }
+
+    pub async fn get_event(&self) -> std::result::Result<Event, flume::RecvError> {
+        self.event_receiver.recv_async().await
+    }
+
+    pub extern "C" fn sub_callback(
+        event_type: std::os::raw::c_int,
+        sym: dxfeed::dxf_const_string_t,
+        data: *const dxfeed::dxf_event_data_t,
+        _data_count: i32, // always 1, and deprecated
+        sender_ptr: *mut std::ffi::c_void,
+    ) {
+        let sender = unsafe { &mut *(sender_ptr as *mut _ as *mut flume::Sender<dxfeed::Event>) };
+        match dxfeed::Event::try_from_c(event_type, sym, data) {
+            Ok(evt) => _ = sender.send(evt),
+            Err(e) => eprintln!("{:?}", e),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct QuoteStreamer {
     host: String,
     token: String,
 
-    connection: Mutex<dxfeed::dxf_connection_t>,
-    subscription: Mutex<dxfeed::dxf_subscription_t>,
-
-    pub event_receiver: flume::Receiver<dxfeed::Event>,
-    event_sender: flume::Sender<dxfeed::Event>,
+    connection: dxfeed::dxf_connection_t,
+    subscriptions: HashMap<SubscriptionId, QuoteSubscription>,
+    next_sub_id: usize,
 }
 
 unsafe impl Send for QuoteStreamer {}
@@ -39,17 +103,14 @@ unsafe impl Send for QuoteStreamer {}
 impl QuoteStreamer {
     pub async fn connect(tasty: &TastyTrade) -> Result<QuoteStreamer> {
         let tokens = tasty.quote_streamer_tokens().await?;
-        let (sender, receiver) = flume::unbounded();
 
-        let this = Self {
+        let mut this = Self {
             host: tokens.streamer_url,
             token: tokens.token,
 
-            connection: Mutex::new(std::ptr::null_mut()),
-            subscription: Mutex::new(std::ptr::null_mut()),
-
-            event_receiver: receiver,
-            event_sender: sender,
+            connection: std::ptr::null_mut(),
+            subscriptions: HashMap::new(),
+            next_sub_id: 0,
         };
 
         let c_host = CString::new(this.host.clone()).unwrap();
@@ -64,7 +125,7 @@ impl QuoteStreamer {
                 None,                     // dxf_socket_thread_creation_notifier_t stcn,
                 None,                     // dxf_socket_thread_destruction_notifier_t stdn,
                 std::ptr::null_mut(),     // void* user_data,
-                &mut *this.connection.lock().unwrap(), // OUT dxf_connection_t* connection);
+                &mut this.connection,     // OUT dxf_connection_t* connection);
             )
         });
 
@@ -87,57 +148,79 @@ impl QuoteStreamer {
         //eprintln!("!!! sub !!! {} => {}", old_status, new_status);
     }
 
-    pub extern "C" fn evt_listener(
-        event_type: std::os::raw::c_int,
-        sym: dxfeed::dxf_const_string_t,
-        data: *const dxfeed::dxf_event_data_t,
-        _data_count: i32, // always 1, and deprecated
-        sender_ptr: *mut std::ffi::c_void,
-    ) {
-        let sender = unsafe { &mut *(sender_ptr as *mut _ as *mut flume::Sender<dxfeed::Event>) };
-        match dxfeed::Event::try_from_c(event_type, sym, data) {
-            Ok(evt) => _ = sender.send(evt),
-            Err(e) => eprintln!("{:?}", e),
-        }
-    }
-
-    pub fn subscribe<S: AsSymbol>(&self, symbols: &[S]) {
-        let sender_ptr: *mut std::ffi::c_void =
-            &self.event_sender as *const flume::Sender<dxfeed::Event> as *mut std::ffi::c_void;
-        let mut sub = *self.subscription.lock().unwrap();
-        let conn = *self.connection.lock().unwrap();
-
-        let symbols: Vec<WideCString> = symbols
-            .iter()
-            .map(|sym| WideCString::from_str(sym.as_symbol().0).unwrap())
-            .collect();
-
-        let mut symbol_pointers: Vec<*const i32> = symbols
-            .iter()
-            .map(|sym| sym.as_ptr() as *const i32)
-            .collect();
-
-        let c_syms: *mut dxfeed::dxf_const_string_t =
-            symbol_pointers.as_mut_slice().as_ptr() as *mut dxfeed::dxf_const_string_t;
-
-        if sub.is_null() {
-            assert_eq!(SUCCESS, unsafe {
-                dxfeed::dxf_create_subscription(
-                    conn,
-                    dxfeed::DXF_ET_QUOTE | dxfeed::DXF_ET_GREEKS,
-                    &mut sub,
-                )
-            });
-        }
-
+    pub fn create_sub(&mut self, flags: i32) -> &QuoteSubscription {
+        let mut subscription: dxfeed::dxf_subscription_t = std::ptr::null_mut();
+        let (event_sender, event_receiver) = flume::unbounded();
+        let event_sender = Box::new(event_sender);
+        let sender_ptr = Box::into_raw(event_sender);
         assert_eq!(SUCCESS, unsafe {
-            dxfeed::dxf_attach_event_listener(sub, Some(Self::evt_listener), sender_ptr)
+            dxfeed::dxf_create_subscription(self.connection, flags, &mut subscription)
+        });
+        assert_eq!(SUCCESS, unsafe {
+            dxfeed::dxf_attach_event_listener(
+                subscription,
+                Some(QuoteSubscription::sub_callback),
+                sender_ptr as *mut std::ffi::c_void,
+            )
         });
 
-        assert_eq!(SUCCESS, unsafe {
-            dxfeed::dxf_add_symbols(sub, c_syms, symbols.len() as i32)
-        });
+        let id = SubscriptionId(self.next_sub_id);
+        self.next_sub_id += 1;
+
+        self.subscriptions.insert(
+            id,
+            QuoteSubscription {
+                id,
+                subscription,
+                event_receiver,
+                sender_ptr,
+            },
+        );
+
+        self.get_sub(id).unwrap()
     }
+
+    pub fn get_sub(&self, id: SubscriptionId) -> Option<&QuoteSubscription> {
+        self.subscriptions.get(&id)
+    }
+
+    // pub fn subscribe<S: AsSymbol>(&self, symbols: &[S]) {
+    //     // let sender_ptr: *mut std::ffi::c_void =
+    //     //     &self.event_sender as *const flume::Sender<dxfeed::Event> as *mut std::ffi::c_void;
+    //     // let mut sub = *self.subscription.lock().unwrap();
+    //     let conn = *self.connection.lock().unwrap();
+
+    //     let symbols: Vec<WideCString> = symbols
+    //         .iter()
+    //         .map(|sym| WideCString::from_str(sym.as_symbol().0).unwrap())
+    //         .collect();
+
+    //     let mut symbol_pointers: Vec<*const i32> = symbols
+    //         .iter()
+    //         .map(|sym| sym.as_ptr() as *const i32)
+    //         .collect();
+
+    //     let c_syms: *mut dxfeed::dxf_const_string_t =
+    //         symbol_pointers.as_mut_slice().as_ptr() as *mut dxfeed::dxf_const_string_t;
+
+    //     if sub.is_null() {
+    //         assert_eq!(SUCCESS, unsafe {
+    //             dxfeed::dxf_create_subscription(
+    //                 conn,
+    //                 dxfeed::DXF_ET_QUOTE | dxfeed::DXF_ET_GREEKS,
+    //                 &mut sub,
+    //             )
+    //         });
+    //     }
+
+    //     assert_eq!(SUCCESS, unsafe {
+    //         dxfeed::dxf_attach_event_listener(sub, Some(Self::evt_listener), sender_ptr)
+    //     });
+
+    //     assert_eq!(SUCCESS, unsafe {
+    //         dxfeed::dxf_add_symbols(sub, c_syms, symbols.len() as i32)
+    //     });
+    // }
 
     // pub async fn handle_events<F>(&self, f: F)
     // where
@@ -148,22 +231,22 @@ impl QuoteStreamer {
     //     }
     // }
 
-    pub async fn get_event(&self) -> std::result::Result<Event, flume::RecvError> {
-        self.event_receiver.recv_async().await
-    }
+    // pub async fn get_event(&self) -> std::result::Result<Event, flume::RecvError> {
+    //     self.event_receiver.recv_async().await
+    // }
 }
 
 impl Drop for QuoteStreamer {
     fn drop(&mut self) {
-        let mut sub = self.subscription.lock().unwrap();
-        let mut conn = self.connection.lock().unwrap();
-        if !sub.is_null() {
-            assert_eq!(SUCCESS, unsafe { dxfeed::dxf_close_subscription(*sub) });
-            *sub = std::ptr::null_mut();
-        }
-        if !conn.is_null() {
-            unsafe { dxfeed::dxf_close_connection(*conn) };
-            *conn = std::ptr::null_mut();
+        // let mut sub = self.subscription.lock().unwrap();
+        self.subscriptions.clear();
+        // if !sub.is_null() {
+        //     assert_eq!(SUCCESS, unsafe { dxfeed::dxf_close_subscription(*sub) });
+        //     *sub = std::ptr::null_mut();
+        // }
+        if !self.connection.is_null() {
+            unsafe { dxfeed::dxf_close_connection(self.connection) };
+            self.connection = std::ptr::null_mut();
         }
     }
 }
